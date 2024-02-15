d90e7a9f3335b582d64aa2c883cacd66faa002ab
Brian Thuo <thuobriank22@gmail.com>
2024-02-15 04:55:54 -0800

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main -a program that prints puts function
+ * Description: This is the main function that prints a message using the puts function. The return value of indicates successful execution.
+ * return: 0 (Success)
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
+
+
